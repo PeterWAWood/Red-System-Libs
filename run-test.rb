f9617c00cffa50bdcbdb2220031cede34359ae77
peter w a wood <peterwawood@gmail.com>
2011-08-20 23:54:32 +0800

    TESTS: improved script to run tests

--- run-test.rb
+++ run-test.rb
@@ -1,17 +1,18 @@
 #!/usr/bin/ruby
 
 # builds and runs Red/System Libs tests
-# it must be run from the dir which contains the testfile
+# requires the file path-to-Red.txt to be in the same directory
 
-puts ARGV[0]
-
+# extract the program name from the source file and
+#  work out where everything is
 source_file = ARGV[0]
-pgm_name = source_file[0, source_file.index('.')]
-current_dir = Dir.getwd
-puts current_dir
-runnable_dir = current_dir + '/Runnable/'
-pgm = runnable_dir + pgm_name
-compiler_dir = IO.read("../../path-to-Red.txt").chomp + 'red-system/'
+pgm_name = File.basename(source_file, '.*')
+pgm_path = File.expand_path(source_file)
+pgm_dir = File.dirname(pgm_path)
+runnable_dir = pgm_dir + '/Runnable/'
+pgm = runnable_dir + '/' + pgm_name
+reds_libs_dir = File.dirname(__FILE__)
+compiler_dir = IO.read(reds_libs_dir + "/path-to-Red.txt").chomp + 'red-system/'
 
 # make runnable directory if needed
 if !File.directory? runnable_dir then
@@ -21,11 +22,12 @@ end
 # change current to red/system compiler directory
 Dir.chdir(compiler_dir)
 
-# compile, relocate execuatble and run
-
-if system('rebol rsc.r %' + current_dir + '/' + source_file) then
+# compile, relocate executable and run
+if system('rebol rsc.r %' + pgm_path) then
   File.rename(compiler_dir + "builds/" + pgm_name, pgm)
   system(pgm)
 end
+
+
 
 
